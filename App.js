cc7d5940cd30970984668f95cdd06dc930063f27
Sean <sean@Seans-MacBook-Pro.local>
2020-10-10 14:54:56 -0700

    added trackign of profit/loss and modularized the code

--- App.js
+++ App.js
@@ -8,14 +8,74 @@ const contains = require('string-contains');
 require('dotenv').config();
 //contains json playerdata after !viewplayers
 const _players = [];
-//contains entire array of [log rows, hand no.] after !parselog
+//contains entire array of [log rows, hand no.] after parseLog() is called
 const _parsed = [];
 
-const getPlayerDataFromLog = (rowstr) => {
+const initState = (row,hand) => {
+    if (contains(row,`joined the game`)) {
+        getPlayerData(row);
+    }
+    if (contains(row, `Player stacks`)) {
+        updatePlayerChips(row)
+    }
+}
+
+const updatePlayerChips = (row, hand) => {
+    let re = new RegExp(/\d+.\d+/g);
+    let results = row.match(re);
+    let i = 0;
+    //profit/loss track
+    let current;
+    let updated;
+    for (i; i < _players.length; i++) {
+        current = _players[i].current_stack;
+        updated = parseFloat(results[i]);
+        const diff = (updated - current);
+        if (hand > 1) {
+            let c = parseFloat(_players[i].net);
+            _players[i].net = (c + diff);
+        }
+        console.log(`${_players[i].game_name} adjusting \$${diff}`);
+        _players[i].current_stack = updated;
+    }
+}
+
+
+const parseLog = (msg) => {
+    const stream = Readable.from(msg.attachments.first().attachment);
+    const raw = [];
+    stream.on('data', (filename) => {
+        https.request(filename, res => {
+            res.on('data', chunk => {
+                raw.push(chunk)
+            });
+            res.on('end', () => {
+                csv.parse(raw.toString(), {headers: false, relax_column_count: true}, (err, data) => {
+                    let hand = 0;
+                    let i;
+                    for (i = data.length-1; i >= 0; i--) {
+                        let row = data[i].toString().split(`,`)[0];
+                        _parsed.push([row,hand]);
+                        if (contains(row, `starting hand`)) {
+                            hand++;
+                        }
+                        initState(row,hand);
+                    }
+                    if (_parsed.length === data.length) {
+                        msg.reply(`Successfully loaded data from \.csv`)
+                    }
+                })
+            });
+        }).end();
+    })
+}
+
+const getPlayerData = (rowstr) => {
     let player = {
         discord_name: '',
         game_name: '',
         id: '',
+        net: 0.0,
         current_stack: 0
     };
     //called when player joins game
@@ -31,6 +91,21 @@ const getPlayerDataFromLog = (rowstr) => {
         console.log(`ADD PLAYER ${player.game_name}\n`);
     }
 }
+
+const viewPlayers = (msg) => {
+    let reply = '\n';
+    if (_players.length != 0) {
+        for (const x of _players) {
+            let replyName = 'unassigned';
+            if (x.discord_name !== '') {replyName = x.discord_name.match(/.+(?=#)/)};
+            reply += `@${replyName} is ${x.game_name} : ${x.id}\n`;
+        }
+    } else {
+        reply += 'No log has been parsed yet\n';
+    }
+    msg.reply(reply);
+}
+
 const assignPlayer = (authortag, ig_name) => {
     let found = undefined;
     for (const x of _players) {
@@ -44,6 +119,7 @@ const assignPlayer = (authortag, ig_name) => {
             discord_name: authortag,
             game_name: found.game_name,
             id: found.id,
+            net: found.net,
             current_stack: found.current_stack
         }
         let i = 0;
@@ -58,62 +134,40 @@ const assignPlayer = (authortag, ig_name) => {
     }
 }
 
+const displayHand = (hand) => {
+    let reply = ``;
+    for (const x of _parsed) {
+        if (x[1] === hand) {
+            reply += x[0] + '\n';
+        }
+    }
+    console.log(`reply ${reply}`)
+    return reply;
+}
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
     if (msg.content === '!viewplayers') {
-        let reply = '\n';
-        if (_players.length != 0) {
-            for (const x of _players) {
-                let replyName = 'unassigned';
-                if (x.discord_name !== '') {replyName = x.discord_name};
-                reply += `@${replyName} is ${x.game_name} : ${x.id}\n`;
-            }
-        } else {
-            reply += 'No log has been parsed yet\n';
-        }
-        msg.reply(reply);
+        viewPlayers(msg);
     }
-    if (msg.content.startsWith('!assignMeTo')) {
-        let args = msg.content.slice('!assignMeTo'.length).trim().split(' ');
+    if (msg.content === '!gethand') {
+        let args = msg.content.slice('!gethand'.length).trim().split(' ');
+        let hand = parseInt(args[0]);
+        let reply = displayHand(hand);
+        console.log(reply);
+        msg.reply(reply)
+    }
+    if (msg.content.startsWith('!setname')) {
+        let args = msg.content.slice('!setname'.length).trim().split(' ');
         assignPlayer(msg.author.tag,args[0])
+        msg.reply(`Assigned ${msg.author.tag.match(/.+(?=#)/)} to ${args[0]} if it exists.`)
     }
     if (msg.content === '!parselog') {
         if (msg.attachments.size > 0) {
-            const stream = Readable.from(msg.attachments.first().attachment)
-            const raw = [];
-            stream.on('data', (filename) => {
-                https.request(filename, res => {
-                    res.on('data', chunk => {
-                        raw.push(chunk)
-                    });
-                    res.on('end', () => {
-                        csv.parse(raw.toString(), {headers: false, relax_column_count: true}, (err, data) => {
-                            let reply = '\n';
-                            let hand = 0;
-                            let i;
-                            for (i = data.length-1; i >= 0; i--) {
-                                let row = data[i].toString();
-                                _parsed.push([row,hand]);
-                                if (contains(row,`joined the game`)) {
-                                    getPlayerDataFromLog(row);
-                                }
-                                if (contains(row, `starting hand`)) {
-                                    hand++;
-                                }
-                                if (contains(row, `Player stacks`)) {
-                                    reply += `hand ${hand} : ${row}\n`;
-                                }
-                            }
-                            if (_parsed.length === data.length) {
-                                msg.reply(reply.slice(0,1900));
-                            }
-                        })
-                    });
-                }).end();
-            })
+            parseLog(msg);
         }
     }
 });
