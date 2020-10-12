93d69a5cf5990f4578ea3c42ce98cad94d524315
009988b <obriens00@icloud.com>
2020-10-12 15:48:16 -0700

    fixedshit

--- App.js
+++ App.js
@@ -11,16 +11,18 @@ const _players = [];
 //contains entire array of [log rows, hand no.] after parseLog() is called
 const _parsed = [];
 
-const initState = (row,hand) => {
+const totalHands = [-1];
+
+const initState = (row,hand,startBal) => {
     if (contains(row,`joined the game`)) {
         getPlayerData(row);
     }
     if (contains(row, `Player stacks`)) {
-        updatePlayerChips(row)
+        updatePlayerChips(row,hand,startBal)
     }
 }
 
-const updatePlayerChips = (row, hand) => {
+const updatePlayerChips = (row, hand, startBal) => {
     let re = new RegExp(/\d+.\d+/g);
     let results = row.match(re);
     let i = 0;
@@ -28,20 +30,25 @@ const updatePlayerChips = (row, hand) => {
     let current;
     let updated;
     for (i; i < _players.length; i++) {
-        current = _players[i].current_stack;
-        updated = parseFloat(results[i]);
-        const diff = (updated - current);
-        if (hand > 1) {
-            let c = parseFloat(_players[i].net);
-            _players[i].net = (c + diff);
+        if (hand > 0) {
+            console.log(`hand: ${hand} player name: ${_players[i].game_name}\n` + _players[i].net[hand-2])
+            current = _players[i].current_stack[hand-1] || startBal;
+            updated = parseFloat(results[i]);
+            const diff = (updated - current);
+            let c = _players[i].net[hand-1];
+            if (c === undefined || c === NaN) {
+                //console.log((`no c so push ${0+diff}`))
+                _players[i].net.push((diff));
+            } else {
+                console.log((`c = ${c.toFixed(2)} so push ${diff.toFixed(2)}`))
+                _players[i].net.push((diff));
+            }
+            _players[i].current_stack.push(updated);
         }
-        console.log(`${_players[i].game_name} adjusting \$${diff}`);
-        _players[i].current_stack = updated;
     }
 }
 
-
-const parseLog = (msg) => {
+const parseLog = (msg, startBal) => {
     const stream = Readable.from(msg.attachments.first().attachment);
     const raw = [];
     stream.on('data', (filename) => {
@@ -51,18 +58,20 @@ const parseLog = (msg) => {
             });
             res.on('end', () => {
                 csv.parse(raw.toString(), {headers: false, relax_column_count: true}, (err, data) => {
-                    let hand = 0;
+                    let hand = -1;
+                    totalHands.push(0);
                     let i;
                     for (i = data.length-1; i >= 0; i--) {
                         let row = data[i].toString().split(`,`)[0];
                         _parsed.push([row,hand]);
                         if (contains(row, `starting hand`)) {
                             hand++;
+                            totalHands[0]++;
                         }
-                        initState(row,hand);
+                        initState(row,hand,startBal);
                     }
                     if (_parsed.length === data.length) {
-                        msg.reply(`Successfully loaded data from \.csv`)
+                        msg.reply(`Successfully loaded data from \.csv\nParsed size: ${_parsed.length}\nHands played: ${totalHands[0]}`)
                     }
                 })
             });
@@ -75,8 +84,8 @@ const getPlayerData = (rowstr) => {
         discord_name: '',
         game_name: '',
         id: '',
-        net: 0.0,
-        current_stack: 0
+        net: [],
+        current_stack: []
     };
     //called when player joins game
     let id_re = new RegExp(/@(.*)"/);
@@ -134,30 +143,36 @@ const assignPlayer = (authortag, ig_name) => {
     }
 }
 
-const displayHand = (hand) => {
-    let reply = ``;
-    for (const x of _parsed) {
-        if (x[1] === hand) {
-            reply += x[0] + '\n';
-        }
-    }
-    console.log(`reply ${reply}`)
-    return reply;
-}
-
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-    if (msg.content === '!viewplayers') {
+    if (msg.content === '!getplayers') {
         viewPlayers(msg);
     }
-    if (msg.content === '!gethand') {
+    if (msg.content.startsWith('!ledger')) {
+        let args = msg.content.slice('!ledger'.length).trim().split(' ');
+        let hand = parseInt(args[0]);
+        let reply = `\n**=== LEDGER - HAND ${hand} ===**\n`;
+        for (const p of _players) {
+            let net = p.net[hand-1];
+            let chips = p.current_stack[hand-1];
+            reply += `> ${p.game_name}:\nchips: ${chips.toFixed(2)}\nwinnings: ${net.toFixed(2)}\n`;
+        }
+        msg.reply(reply);
+    }
+    if (msg.content.startsWith('!gethand')) {
+        console.log(`Parsed size: ${_parsed.length}`);
         let args = msg.content.slice('!gethand'.length).trim().split(' ');
         let hand = parseInt(args[0]);
-        let reply = displayHand(hand);
-        console.log(reply);
+        let reply = ``;
+        for (const row of _parsed) {
+            if (row[1] === hand) {
+                if (contains(row[0], `Player stacks`))
+                reply += row[0] + '\n';
+            }
+        }
         msg.reply(reply)
     }
     if (msg.content.startsWith('!setname')) {
@@ -165,9 +180,12 @@ client.on('message', msg => {
         assignPlayer(msg.author.tag,args[0])
         msg.reply(`Assigned ${msg.author.tag.match(/.+(?=#)/)} to ${args[0]} if it exists.`)
     }
-    if (msg.content === '!parselog') {
+    if (msg.content.startsWith('!parselog')) {
+        let args = msg.content.slice('!parselog'.length).trim().split(' ');
+        let startBal = parseInt(args[0])
+        console.log(`startingBalance: ` + startBal)
         if (msg.attachments.size > 0) {
-            parseLog(msg);
+            parseLog(msg, startBal);
         }
     }
 });
